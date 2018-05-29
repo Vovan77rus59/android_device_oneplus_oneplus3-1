daafdd7107b694ecd73a9ac01787ed8f2a71a2bc
padarshr <padarshr@codeaurora.org>
2018-05-29 13:34:14 +0530

    op3: Enable System-As-Root behaviour for non-A/B as well.
    Change-Id: I19b8b8342dc634629427c409764f6cc7d69235d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,8 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loop.max_part=7 swiotlb=2048
+BOARD_KERNEL_CMDLINE += ro root=/dev/sde20 rootwait skip_initramfs init=/init
+BOARD_KERNEL_CMDLINE += dm=\"system none ro,0 1 android-verity /dev/sde20\"
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
@@ -186,6 +188,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3154116608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 57436708864
 BOARD_FLASH_BLOCK_SIZE := 262144
 TARGET_USES_MKE2FS := true
+
+# Enable System As Root even for non-A/B from Q onwards
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
