87dfd50159d84b2f4ae763595e501ab48758c767
ZVNexus <zvnexus@outlook.com>
2019-08-30 10:54:24 -0400

    op3: DU

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_oneplus3.mk
+	$(LOCAL_DIR)/du.mk
--- du.mk
+++ du.mk
@@ -18,7 +18,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_oneplus3
+# Inherit from device configuration
+$(call inherit-product, device/oneplus/oneplus3/device.mk)
+
+# Inherit DU product configuration
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Inherit from PixelGapps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
+## IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
+PRODUCT_NAME := du_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := OnePlus 3
@@ -26,5 +41,7 @@ PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_COPY_FILES += device/oneplus/oneplus3/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
-$(call inherit-product, device/oneplus/oneplus3/device.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys"
 
