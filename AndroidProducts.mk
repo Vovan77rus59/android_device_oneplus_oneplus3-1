e75ecb130b62f85b85a1d7d3c8485a8ea40bd44c
ZVNexus <zvnexus@outlook.com>
2019-09-01 21:28:32 -0400

    op3: AOSP
    Change-Id: I4d91de7bb5000c1769873fc54c62d3bccce737ab

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 StatiXOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oneplus3.mk
+    $(LOCAL_DIR)/statix_oneplus3.mk
+
+COMMON_LUNCH_CHOICES := \
+    statix_oneplus3-user
+    statix_oneplus3-userdebug
+    statix_oneplus3-eng
