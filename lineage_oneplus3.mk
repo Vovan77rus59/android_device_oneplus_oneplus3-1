7af0011f5b9cec95e183791370e0aba82cffe8e2
ZVNexus <zvnexus@outlook.com>
2019-09-06 09:20:03 -0400

    op3: inherit product api level makefile
    Change-Id: I970ef7f9a0d352a286395c7cad79dcaf3245f0ab
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- lineage_oneplus3.mk
+++ lineage_oneplus3.mk
@@ -17,6 +17,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Product launched with Marshmellow
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
