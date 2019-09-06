aac94a3576b865ba3400ab599af0ab1ff3ca21d6
ZVNexus <zvnexus@outlook.com>
2019-09-05 20:39:50 -0400

    op3: data-ipa-cfg-mgr: Silence -Wimplicit-fallthrough warnings

--- data-ipa-cfg-mgr/ipacm/src/IPACM_Main.cpp
+++ data-ipa-cfg-mgr/ipacm/src/IPACM_Main.cpp
@@ -609,7 +609,7 @@ void* ipa_driver_msg_notifier(void *param)
 
 		case WLAN_SWITCH_TO_SCC:
 			IPACMDBG_H("Received WLAN_SWITCH_TO_SCC\n");
-		case WLAN_WDI_ENABLE:
+		U_FALLTHROUGH; case WLAN_WDI_ENABLE:
 			IPACMDBG_H("Received WLAN_WDI_ENABLE\n");
 			if (IPACM_Iface::ipacmcfg->isMCC_Mode == true)
 			{
@@ -620,7 +620,7 @@ void* ipa_driver_msg_notifier(void *param)
 			continue;
 		case WLAN_SWITCH_TO_MCC:
 			IPACMDBG_H("Received WLAN_SWITCH_TO_MCC\n");
-		case WLAN_WDI_DISABLE:
+		U_FALLTHROUGH; case WLAN_WDI_DISABLE:
 			IPACMDBG_H("Received WLAN_WDI_DISABLE\n");
 			if (IPACM_Iface::ipacmcfg->isMCC_Mode == false)
 			{
