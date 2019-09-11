e4d9467682b3850455038ee40f581651c36d0529
ZVNexus <zvnexus@outlook.com>
2019-09-10 23:20:07 -0400

    op3: Don't build PocketMode, Doze, or inherit OPPO repo.
    PocketMode will be fixed and will stay, Doze will be removed for
    CustomDoze, and OPPO repo will be trimmed down for OnePlus Extras.
    
    Change-Id: Ie836c356c3dceeb33a5a74401d668b48cdbc3d11

--- device.mk
+++ device.mk
@@ -237,10 +237,6 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -249,7 +245,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    OneplusPocketMode \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Gatekeeper HAL
@@ -498,6 +493,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
-
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
