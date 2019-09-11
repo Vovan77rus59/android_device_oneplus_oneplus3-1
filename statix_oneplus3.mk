2c39eda3a2bac6fe087ab14f4dc23613d1034df7
ZVNexus <zvnexus@outlook.com>
2019-09-11 19:52:30 -0400

    HAX

--- statix_oneplus3.mk
+++ statix_oneplus3.mk
@@ -20,13 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit DU
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := du_oneplus3
+PRODUCT_NAME := statix_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
